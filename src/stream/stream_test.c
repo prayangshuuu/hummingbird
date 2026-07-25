988426271bc432e72f01715d868e0fc5a5ed0259
Prayangshu Biswas <prayangshuuu@gmail.com>
2026-07-26 01:10:00 +0600

    fix: compiler warnings and errors

--- src/stream/stream_test.c
+++ src/stream/stream_test.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-static void test_memory_manager() {
+static void test_memory_manager(void) {
     size_t capacities[HB_TIER_COUNT] = {1024, 4096, 8192, 0};
     hbi_memory_manager_t *mm = hbi_memory_manager_create(capacities);
     assert(mm != NULL);
@@ -11,36 +11,45 @@ static void test_memory_manager() {
     // Allocate 512 bytes on Tier 1
     bool ok = hbi_memory_manager_allocate(mm, 1, 512, HB_TIER_1);
     assert(ok);
-    assert(hbi_memory_manager_get_residency(mm, 1) == HB_RESIDENCY_RESIDENT);
+    (void)ok;
+    bool resident = (hbi_memory_manager_get_residency(mm, 1) == HB_RESIDENCY_RESIDENT);
+    assert(resident);
+    (void)resident;
 
     // Move to Tier 0
     ok = hbi_memory_manager_move(mm, 1, HB_TIER_0);
     assert(ok);
+    (void)ok;
 
     hbi_memory_manager_destroy(mm);
     printf("[ok] memory_manager\n");
 }
 
-static void test_cache_manager() {
+static void test_cache_manager(void) {
     hbi_cache_manager_t *cache = hbi_cache_manager_create(HB_CACHE_POLICY_LRU, 4096);
     assert(cache != NULL);
 
     hbi_cache_manager_insert(cache, 1);
     hbi_cache_manager_insert(cache, 2);
 
-    assert(hbi_cache_manager_access(cache, 1) == true);
-    assert(hbi_cache_manager_access(cache, 3) == false);
+    bool access1 = hbi_cache_manager_access(cache, 1);
+    assert(access1 == true);
+    (void)access1;
+    bool access3 = hbi_cache_manager_access(cache, 3);
+    assert(access3 == false);
+    (void)access3;
 
     hbi_block_id_t evicted;
     bool evict_ok = hbi_cache_manager_evict(cache, &evicted);
     assert(evict_ok);
+    (void)evict_ok;
     assert(evicted == 2); // 2 is LRU because 1 was accessed
 
     hbi_cache_manager_destroy(cache);
     printf("[ok] cache_manager\n");
 }
 
-static void test_scheduler() {
+static void test_scheduler(void) {
     size_t capacities[HB_TIER_COUNT] = {1024, 1024, 8192, 0};
     hbi_memory_manager_t *mm = hbi_memory_manager_create(capacities);
     hbi_cache_manager_t *cache = hbi_cache_manager_create(HB_CACHE_POLICY_LRU, 1024);
@@ -49,13 +58,16 @@ static void test_scheduler() {
 
     bool load_ok = hbi_scheduler_load(sched, 100, 512);
     assert(load_ok);
+    (void)load_ok;
 
     bool load_ok2 = hbi_scheduler_load(sched, 101, 512);
     assert(load_ok2);
+    (void)load_ok2;
 
     // Next load should evict block 100
     bool load_ok3 = hbi_scheduler_load(sched, 102, 512);
     assert(load_ok3);
+    (void)load_ok3;
 
     hbi_scheduler_destroy(sched);
     hbi_cache_manager_destroy(cache);
