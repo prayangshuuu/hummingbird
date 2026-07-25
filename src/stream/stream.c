16d9e550644e73ec88d487a516a745a13f37ddd7
Prayangshu Biswas <prayangshuuu@gmail.com>
2026-07-26 01:10:00 +0600

    fix: compiler warnings and errors

--- src/stream/stream.c
+++ src/stream/stream.c
@@ -9,19 +9,19 @@ hbi_status hbi_stream_selftest(void) {
     size_t capacities[HB_TIER_COUNT] = {1024, 4096, 8192, 0};
     hbi_memory_manager_t *mm = hbi_memory_manager_create(capacities);
     if (!mm)
-        return HBI_ERROR_UNKNOWN;
+        return HBI_ERR_INTERNAL;
 
     hbi_cache_manager_t *cache = hbi_cache_manager_create(HB_CACHE_POLICY_LRU, 4096);
     if (!cache) {
         hbi_memory_manager_destroy(mm);
-        return HBI_ERROR_UNKNOWN;
+        return HBI_ERR_INTERNAL;
     }
 
     hbi_scheduler_t *sched = hbi_scheduler_create(mm, cache);
     if (!sched) {
         hbi_cache_manager_destroy(cache);
         hbi_memory_manager_destroy(mm);
-        return HBI_ERROR_UNKNOWN;
+        return HBI_ERR_INTERNAL;
     }
 
     hbi_scheduler_destroy(sched);
--- src/stream/tracker.c
+++ src/stream/tracker.c
@@ -18,27 +18,32 @@ void hbi_locality_tracker_destroy(hbi_locality_tracker_t *tracker) {
 }
 
 void hbi_locality_tracker_record_hit(hbi_locality_tracker_t *tracker, hbi_block_id_t id) {
+    (void)id;
     if (tracker)
         tracker->stats.hits++;
 }
 
 void hbi_locality_tracker_record_miss(hbi_locality_tracker_t *tracker, hbi_block_id_t id) {
+    (void)id;
     if (tracker)
         tracker->stats.misses++;
 }
 
 void hbi_locality_tracker_record_transfer(hbi_locality_tracker_t *tracker, hbi_block_id_t id,
                                           size_t bytes) {
+    (void)id;
     if (tracker)
         tracker->stats.bytes_transferred += bytes;
 }
 
 void hbi_locality_tracker_record_promotion(hbi_locality_tracker_t *tracker, hbi_block_id_t id) {
+    (void)id;
     if (tracker)
         tracker->stats.promotions++;
 }
 
 void hbi_locality_tracker_record_demotion(hbi_locality_tracker_t *tracker, hbi_block_id_t id) {
+    (void)id;
     if (tracker)
         tracker->stats.demotions++;
 }
@@ -51,7 +56,7 @@ hbi_locality_stats_t hbi_locality_tracker_get_stats(hbi_locality_tracker_t *trac
     // Calculate temporal locality score simply as hit rate for this milestone
     uint64_t total = tracker->stats.hits + tracker->stats.misses;
     if (total > 0) {
-        tracker->stats.temporal_locality_score = (float)tracker->stats.hits / total;
+        tracker->stats.temporal_locality_score = (float)tracker->stats.hits / (float)total;
     } else {
         tracker->stats.temporal_locality_score = 0.0f;
     }
